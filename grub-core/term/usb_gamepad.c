dca05974c78276dcd251930f95c3defe6eb78a95
rexim <reximkut@gmail.com>
2019-07-17 00:39:47 +0700

    khooy -> usb_gamepad

--- grub-core/term/usb_gamepad.c
+++ grub-core/term/usb_gamepad.c
@@ -11,7 +11,7 @@ GRUB_MOD_LICENSE ("GPLv3");
 #define USB_HID_SET_IDLE	0x0A
 #define USB_HID_SET_PROTOCOL	0x0B
 
-struct grub_usb_khooy_data
+struct grub_usb_gamepad_data
 {
     grub_usb_device_t usbdev;
     int configno;
@@ -22,9 +22,9 @@ struct grub_usb_khooy_data
 };
 
 static int
-khooy_getkey (struct grub_term_input *term)
+usb_gamepad_getkey (struct grub_term_input *term)
 {
-    struct grub_usb_khooy_data *termdata = term->data;
+    struct grub_usb_gamepad_data *termdata = term->data;
     grub_size_t actual;
 
     grub_usb_err_t err = grub_usb_check_transfer (termdata->transfer, &actual);
@@ -33,7 +33,7 @@ khooy_getkey (struct grub_term_input *term)
         return GRUB_TERM_NO_KEY;
     }
 
-    grub_dprintf("khooy",
+    grub_dprintf("usb_gamepad",
                  "Received report: %x %x %x %x %x %x %x %x\n",
                  termdata->report[0],
                  termdata->report[1],
@@ -61,31 +61,31 @@ khooy_getkey (struct grub_term_input *term)
 }
 
 static int
-khooy_getkeystatus (struct grub_term_input *term __attribute__ ((unused)))
+usb_gamepad_getkeystatus (struct grub_term_input *term __attribute__ ((unused)))
 {
     return 0;
 }
 
-static struct grub_term_input khooy_input_term =
+static struct grub_term_input usb_gamepad_input_term =
   {
-    .name = "khooy",
-    .getkey = khooy_getkey,
-    .getkeystatus = khooy_getkeystatus
+    .name = "usb_gamepad",
+    .getkey = usb_gamepad_getkey,
+    .getkeystatus = usb_gamepad_getkeystatus
   };
 
 static int
-grub_usb_khooy_attach(grub_usb_device_t usbdev, int configno, int interfno)
+grub_usb_gamepad_attach(grub_usb_device_t usbdev, int configno, int interfno)
 {
-    // TODO: grub_usb_khooy_attach leaks memory every time you connect a new USB device
-    grub_dprintf("khooy", "Khooy configno: %d, interfno: %d\n", configno, interfno);
-    struct grub_usb_khooy_data *data = grub_malloc(sizeof(struct grub_usb_khooy_data));
+    // TODO: grub_usb_gamepad_attach leaks memory every time you connect a new USB device
+    grub_dprintf("usb_gamepad", "Usb_Gamepad configno: %d, interfno: %d\n", configno, interfno);
+    struct grub_usb_gamepad_data *data = grub_malloc(sizeof(struct grub_usb_gamepad_data));
     struct grub_usb_desc_endp *endp = NULL;
     if (!data) {
         grub_print_error();
         return 0;
     }
 
-    grub_dprintf("khooy", "Endpoints: %d\n",
+    grub_dprintf("usb_gamepad", "Endpoints: %d\n",
                  usbdev->config[configno].interf[interfno].descif->endpointcnt);
 
     int j = 0;
@@ -103,13 +103,13 @@ grub_usb_khooy_attach(grub_usb_device_t usbdev, int configno, int interfno)
     if (j == usbdev->config[configno].interf[interfno].descif->endpointcnt)
         return 0;
 
-    grub_dprintf ("khooy", "HID Khooy found! Endpoint: %d\n", j);
+    grub_dprintf ("usb_gamepad", "HID Usb_Gamepad found! Endpoint: %d\n", j);
 
     data->usbdev = usbdev;
     data->configno = configno;
     data->interfno = interfno;
     data->endp = endp;
-    khooy_input_term.data = data;
+    usb_gamepad_input_term.data = data;
 
     data->transfer = grub_usb_bulk_read_background (
         usbdev,
@@ -123,7 +123,7 @@ grub_usb_khooy_attach(grub_usb_device_t usbdev, int configno, int interfno)
         return 0;
     }
 
-    grub_term_register_input("khooy", &khooy_input_term);
+    grub_term_register_input("usb_gamepad", &usb_gamepad_input_term);
 
     return 0;
 }
@@ -131,16 +131,16 @@ grub_usb_khooy_attach(grub_usb_device_t usbdev, int configno, int interfno)
 static struct grub_usb_attach_desc attach_hook =
 {
     .class = GRUB_USB_CLASS_HID,
-    .hook = grub_usb_khooy_attach
+    .hook = grub_usb_gamepad_attach
 };
 
-GRUB_MOD_INIT(khooy)
+GRUB_MOD_INIT(usb_gamepad)
 {
-    grub_dprintf("khooy", "Khooy module loaded\n");
+    grub_dprintf("usb_gamepad", "Usb_Gamepad module loaded\n");
     grub_usb_register_attach_hook_class(&attach_hook);
 }
 
-GRUB_MOD_FINI(khooy)
+GRUB_MOD_FINI(usb_gamepad)
 {
-    grub_dprintf("khooy", "Khooy fini-ed\n");
+    grub_dprintf("usb_gamepad", "Usb_Gamepad fini-ed\n");
 }
