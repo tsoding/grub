f76196bab5aefc4f9258fb5d760ff956eee4ed19
rexim <reximkut@gmail.com>
2019-07-31 02:53:11 +0700

    Remove dead macros
    https://github.com/tsoding/grub-gamepad/issues/19

--- grub-core/term/usb_gamepad.c
+++ grub-core/term/usb_gamepad.c
@@ -5,13 +5,6 @@
 
 GRUB_MOD_LICENSE ("GPLv3");
 
-#define USB_HID_GET_REPORT	0x01
-#define USB_HID_GET_IDLE	0x02
-#define USB_HID_GET_PROTOCOL	0x03
-#define USB_HID_SET_REPORT	0x09
-#define USB_HID_SET_IDLE	0x0A
-#define USB_HID_SET_PROTOCOL	0x0B
-
 typedef enum {
     DPAD_UP = 0x0,
     DPAD_UPRIGHT,
